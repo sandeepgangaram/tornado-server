05f941464c51f3216521f20c760173ba864ff0ed
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-08-12 22:22:56 +0530

    intialize app and listen on port

---
+++ index.py
@@ -0,0 +1,15 @@
+import tornado.web;
+import tornado.ioloop;
+
+class basicRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, World!")
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/",basicRequestHandler)
+    ])
+
+    port = 8882;
+    app.listen(port);
+    print(f"Listening on port {port}");
